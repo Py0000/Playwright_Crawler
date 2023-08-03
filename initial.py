50f6c3307ec6108299fc43780b9d0c291e81d314
Py0000 <pylim8266@gmail.com>
2023-08-03 17:26:47 +0800

    Test out retrieving html content feature

--- initial.py
+++ initial.py
@@ -1,36 +1,58 @@
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
+import time
 
 # sync_playwright() returns a sync Playwright Object
 # with ... as ... ensures proper resource management & close the Playwright instance at the end 
 
+def test_check_user_agent(page):
+    user_agent = page.evaluate('''() => window.navigator.userAgent''')
+    print("User-Agent:", user_agent)
+
+def test_check_referrer(page):
+    referrer = page.evaluate('''() => document.referrer''')
+    print("Referrer:", referrer)
+
+
 with sync_playwright() as p:
+    
     custom_user_agent = "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/W.X.Y.Z Safari/537.36"
-    custom_referrer = "https://www.facebook.com/"
+    custom_referer = "https://www.facebook.com/"
+
     custom_http_header = [
-        f"--user-agent={custom_user_agent}", 
-        f"--referrer={custom_referrer}",
+        # f"--user-agent={custom_user_agent}", 
+        f"--referer={custom_referer}",
     ]
 
     browser = p.chromium.launch(headless=False, slow_mo=50, args=custom_http_header)
-    
+
+
+    # browser = p.chromium.launch(headless=False, slow_mo=50)
+
     # creates a new page within the browser
     page = browser.new_page()
 
     # Set the user-agent header
-    page.set_extra_http_headers({"User-Agent": custom_user_agent})
+    # page.set_extra_http_headers({"User-Agent": custom_user_agent})
 
     # Set the referer header
-    page.set_extra_http_headers({"referer": custom_referrer})
+    page.set_extra_http_headers({"referer": custom_referer})
 
     # visit the url 
     page.goto("https://www.google.com.sg/") 
+    time.sleep(5)
 
     # Get the user-agent string
-    user_agent = page.evaluate('''() => window.navigator.userAgent''')
-    print("User-Agent:", user_agent)
+    test_check_user_agent(page)
 
     # Get the referrer
-    referrer = page.evaluate('''() => document.referrer''')
-    print("Referrer:", referrer)
+    test_check_referrer(page)
+
+    # Wait for the page to load completely (wait for the load event)
+    page.wait_for_load_state('load')
+
+    # Get the HTML content of the page
+    html_content = page.content()
+    soup = BeautifulSoup(html_content, "lxml")
+    print(soup.prettify())
 
