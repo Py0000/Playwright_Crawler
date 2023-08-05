1f3b166de1bcc1b92430bc4aec7d2baee3031783
Py0000 <pylim8266@gmail.com>
2023-08-05 15:29:43 +0800

    Test crawling embedded link

--- initial.py
+++ initial.py
@@ -6,7 +6,6 @@ from bs4 import BeautifulSoup
 
 import user_actions as action
 import utility as util
-import utility_logging as util_log
 
 # sync_playwright() returns a sync Playwright Object
 # with ... as ... ensures proper resource management & close the Playwright instance at the end 
@@ -25,6 +24,76 @@ def add_time_for_page_to_load(page):
         pass
 
 
+
+def get_html_content(page, base_folder_name, formatted_index, actual_url, embedded_flag):
+    # Can add mouse movement here
+
+    screenshot_path = util.get_screenshot_file_path(base_folder_name, formatted_index)
+    action.save_screenshot(page, screenshot_path)
+    print("Screenshot Captured...")
+
+    # Can add page scroll here
+
+    html_content = page.content()
+    soup = BeautifulSoup(html_content, "lxml")
+    scraped_url = page.url
+    util.detect_redirection(base_folder_name, scraped_url, actual_url)
+    util.save_crawled_url(base_folder_name, scraped_url)
+
+    embedded_file_path = ""
+    if not embedded_flag:
+        embedded_file_path = util.get_embedded_links(base_folder_name, soup, page, actual_url)
+    
+    util.get_all_html_tags(base_folder_name, soup, formatted_index)
+
+    print("Actual url: ", actual_url)
+    print("Url visited: ", scraped_url)
+    util.test_check_user_agent(page)
+    util.test_check_referrer(page)
+
+    return soup.prettify(), embedded_file_path
+
+
+def scrape_content(page, base_folder_name, referer_url, actual_url, formatted_index, embedded_flag):
+    # If it is the seed url, visit google first
+    # Then inject a javascript click-through to visit the seed url to ensure no empty referrer
+    if not embedded_flag:
+        referer_url = referer_url + actual_url
+
+    # Set the referrer first
+    page.goto(referer_url) 
+    add_time_for_page_to_load(page)
+
+     # Visit the actual webpage
+    page.evaluate('window.location.href = "{}";'.format(actual_url))
+    add_time_for_page_to_load(page)
+
+    return get_html_content(page, base_folder_name, formatted_index, actual_url, embedded_flag)
+
+
+def crawl_depth_one_embedded_links(page, embedded_link_path, index, error_list, base_folder_name, referrer):
+    url_list = util.get_level_one_embedded_link(embedded_link_path)
+
+    for url in url_list:
+        formatted_index_str = util.format_index_base_file_name(index)
+        try:
+            content, _ = scrape_content(page, base_folder_name, referrer, url, formatted_index_str, embedded_flag=True)
+
+            if content is not None:
+                util.save_html_script(base_folder_name, content, formatted_index_str)
+        
+        except Exception as e:
+            util.save_html_script(base_folder_name, f"Error occurred for url: {url}\n{e}", formatted_index_str)
+            util.save_crawled_url(base_folder_name, util.ERROR_URL_FLAG)
+            error_list.append(index)
+            continue
+
+        index += 1
+        time.sleep(random.randint(5, 10))
+            
+    return index, error_list
+
+
 def get_dataset(page, base_folder_name, url_list):
     error_list = []
 
@@ -32,31 +101,21 @@ def get_dataset(page, base_folder_name, url_list):
     for url in url_list:
         formatted_index_str = util.format_index_base_file_name(index)
 
-        # visit the url 
-        page.goto(url) 
-        add_time_for_page_to_load(page)
-
         try:
-            # Get the HTML content of the page
-            html_content = page.content()
-            soup = BeautifulSoup(html_content, "lxml")
-
-            embedded_path = util.get_embedded_links(base_folder_name, soup, page, url)
-
-            # Get the user-agent & referrer string
-            util_log.test_check_user_agent(page)
-            util_log.test_check_referrer(page)
-
+            referrer = util.GOOGLE_SEARCH_QUERY_REFERRER
+            content, embedded_path = scrape_content(page, base_folder_name, referrer, url, formatted_index_str, embedded_flag=False)
             index += 1
 
-            if soup is not None:
-                util.save_html_script(base_folder_name, soup.prettify(), formatted_index_str)
+            if content is not None:
+                util.save_html_script(base_folder_name, content, formatted_index_str)
                 
-
-                #Scrape embedded link
+                # Scrape embedded link
+                referrer = url
+                index, error_list = crawl_depth_one_embedded_links(page, embedded_path, index, error_list, base_folder_name, referrer)
             
         except Exception as e:
             util.save_html_script(base_folder_name, f"Error occurred for url: {url}\n{e}", formatted_index_str)
+            util.save_crawled_url(base_folder_name, util.ERROR_URL_FLAG)
             error_list.append(index)
             index += 1
             continue
@@ -74,8 +133,6 @@ def get_dataset(page, base_folder_name, url_list):
     #action.move_mouse_smoothly_top_left_bottom_right(page)
 
     #action.mouse_click(page, 'left')
-
-    #action.save_screenshot(page, "test.png")
 
     #action.page_scroll(page)
 
@@ -99,7 +156,7 @@ def crawl(url_list):
 
     custom_http_header = [
         f"--user-agent={util.DESKTOP_USER_AGENT}", 
-        f"--referer={util.GOOGLE_REFERRER}",
+        # f"--referer={util.GOOGLE_REFERRER}",
     ]
 
     p = sync_playwright().start()
@@ -110,7 +167,7 @@ def crawl(url_list):
 
     # Set the user-agent & referrer header
     page.set_extra_http_headers({"User-Agent": util.DESKTOP_USER_AGENT})
-    page.set_extra_http_headers({"referer": util.GOOGLE_REFERRER})
+    # page.set_extra_http_headers({"referer": util.GOOGLE_REFERRER})
 
     # Can add setting the config for mobile/desktop/bots
 
@@ -119,6 +176,10 @@ def crawl(url_list):
     sub_folder_list = [
         util.CRAWLED_HTML_SCRIPT_FOLDER,
         util.CRAWLED_EMBEDDED_LINK_FOLDER,
+        util.CRAWLED_PAGE_SCREENSHOT_FOLDER,
+        util.CRAWLED_URL_FOLDER,
+        util.CRAWLED_HTML_TAG_FOLDER,
+        util.CRAWLED_REDIRECTION_FOLDER,
     ]
 
     util.generate_folder_for_crawling(base_folder_name, sub_folder_list)
@@ -131,4 +192,4 @@ def crawl(url_list):
     print("\nCrawling done...")
 
 
-crawl(["https://www.google.com.sg/"])
+crawl(["https://www.youtube.com/"])
--- utility.py
+++ utility.py
@@ -4,16 +4,55 @@ import os
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
+ERROR_URL_FLAG = "ERROR_URL"
 
 CRAWLED_DATA_IDENTIFIER = "crawled_dataset"
 CRAWLED_HTML_SCRIPT_FOLDER = "crawled_html_scripts"
 CRAWLED_EMBEDDED_LINK_FOLDER = "crawled_embedded_links"
+CRAWLED_PAGE_SCREENSHOT_FOLDER = "crawled_screenshots"
+CRAWLED_URL_FOLDER = "crawled_urls"
+CRAWLED_HTML_TAG_FOLDER = "crawled_html_tags"
+CRAWLED_REDIRECTION_FOLDER = "crawled_redirected_url"
 
 DESKTOP_BOT_AGENT = "Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Chrome/W.X.Y.Z Safari/537.36"
 DESKTOP_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
 
+GOOGLE_SEARCH_QUERY_REFERRER = "https://www.google.com.sg/search?q="
 FACEBOOK_REFERRER = "https://www.facebook.com/"
 GOOGLE_REFERRER = "https://www.google.com/"
+
+CURRENT_COVERED_TAG_SET = {'title', 
+                           'form', 'input', 'textarea', 'button', 'select', 'output',
+                           'iframe',
+                           'style', 'span', 'hr',
+                           'img', 'audio', 'video', 'svg', 'picture', 'source', 'track', 'map', 'canvas',
+                           'a', 'link', 'nav',
+                           'script', 'noscript', 'embed', 'object', 'code',
+                           'ul', 'ol', 'dl', 'dt', 'dd',
+                           'table',
+                           'head', 'meta', 'base', 'bpdy', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'body',
+                           'div', 'header', 'footer', 'main', 'section', 'article', 'aside', 'details', 'dialog', 'data',
+                           'br', 'html',
+                           'abbr', 'b', 'bdi', 'bdo', 'blockquote', 'cite', 'del', 'dfn', 'em', 'i', 'ins', 'kbd',
+                           'mark', 'pre', 'q', 's', 'small', 'samp', 'strong', 'sup', 'sub', 'u', 'var',
+                           'meter', 'progress',
+                           'template',
+                        }
+
+CURRENT_KNOWN_EXCLUEDED_TAG_SET = {
+    'optgroup', 'option', 'label', 'fieldset', 'legend', 'datalist', 'area', 'figcaption', 'figure', 
+    'li', 'caption', 'th', 'tr', 'td', 'thead', 'tbody', 'tfoot', 'col', 'colgroup', 'summary', 'param',
+}
+
+
+def test_check_user_agent(page):
+    user_agent = page.evaluate('''() => window.navigator.userAgent''')
+    print("User-Agent:", user_agent)
+
+def test_check_referrer(page):
+    referrer = page.evaluate('''() => document.referrer''')
+    print("Referrer:", referrer)
+
 
 
 def format_index_base_file_name(index):
@@ -62,7 +101,7 @@ def get_links_in_iframe(soup, file, url, page, added_url_set):
             continue
 
         page.goto(iframe_src)
-        iframe_soup = BeautifulSoup(page.page_source, 'lxml')
+        iframe_soup = BeautifulSoup(page.content(), 'lxml')
 
         for a in iframe_soup.find_all('a'):
             added_url_set = save_unique_embedded_url(file, a, url, added_url_set)
@@ -82,6 +121,15 @@ def get_embedded_links(base_folder_name, soup, page, url):
 
     return path
 
+def get_level_one_embedded_link(file_path):
+    url_list = []
+    
+    with open(file_path, 'r') as file:
+        for line in file:
+            url = line.strip()
+            url_list.append(url)
+           
+    return url_list
 
 
 def save_html_script(base_folder_name, text, name):
@@ -90,3 +138,46 @@ def save_html_script(base_folder_name, text, name):
     with open(file, "w", encoding="utf-8") as f:
         f.write(text)
     
+
+
+def save_crawled_url(base_folder_name, url):
+    file_name = "urls.txt"
+    file = os.path.join(os.getcwd(), base_folder_name, CRAWLED_URL_FOLDER, file_name)
+    
+    with open(file, "a") as f:
+        f.write(url + '\n')
+
+
+
+def get_screenshot_file_path(base_folder, file_name):
+    folder_path = CRAWLED_PAGE_SCREENSHOT_FOLDER
+    file_name = f"{file_name}.png"
+    file_path = os.path.join(base_folder, folder_path, file_name)
+
+    return file_path
+
+
+
+def get_all_html_tags(base_folder_name, soup, index):
+    file_name = index + ".txt"
+    file = os.path.join(os.getcwd(), base_folder_name, CRAWLED_HTML_TAG_FOLDER, file_name)
+
+    set = {tag.name for tag in soup.find_all()}
+    initial_diff = set.difference(CURRENT_COVERED_TAG_SET)
+    diff = initial_diff.difference(CURRENT_KNOWN_EXCLUEDED_TAG_SET)
+
+    if len(diff) == 0:
+        diff = ""
+
+    with open(file, "a") as f:
+        f.write(str(diff))
+
+
+def detect_redirection(base_folder_name, current_url, actual_url):
+    file_name = "redirection.txt"
+    file = os.path.join(os.getcwd(), base_folder_name, CRAWLED_REDIRECTION_FOLDER, file_name)
+
+    if current_url != actual_url:
+        with open(file, "a") as f:
+            text = f"--------------------------------------------------------------\nActual URL: {actual_url}\nRedirected URL: {current_url}\n-------------------------------------------------------------\n\n"
+            f.write(text)
