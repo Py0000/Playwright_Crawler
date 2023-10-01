cff1ca146580007ccea5eb2b91968f8d631043d0
Py0000 <pylim8266@gmail.com>
2023-10-01 23:02:59 +0800

    refactor data_reallocator code

--- Crawler_Dataset/data_reallocator.py
+++ Crawler_Dataset/data_reallocator.py
@@ -6,9 +6,15 @@ import time
 import network_data_processor
 
 def zip_folder(dest_folder, folder):
-    output_filename = os.path.join(dest_folder, folder)
-    shutil.make_archive(output_filename, "zip", dest_folder, folder) # Zip the folder
-    shutil.rmtree(os.path.join(dest_folder, folder)) # Remove the orginal folder
+    try:
+        output_filename = os.path.join(dest_folder, folder)
+        shutil.make_archive(output_filename, "zip", dest_folder, folder) # Zip the folder
+        shutil.rmtree(os.path.join(dest_folder, folder)) # Remove the orginal folder
+        print(f"Successfully zipped {folder}")
+    except Exception as e:
+        print(f"Error occurred when zipping {folder}: {e}")
+
+
 
 
 def move_folder(src_folder, dest_folder):
@@ -34,14 +40,11 @@ def move_folder(src_folder, dest_folder):
                     print(f"Error occured for {src}: ", e)
                 
                 finally:
-                    try:
-                        zip_folder(dest_folder, folder)
-                        print(f"Successfully zipped {folder}")
+                    zip_folder(dest_folder, folder)
                         
-                    except Exception as e:
-                        print(f"Error occurred when zipping: {folder}")
+                        
                     
-
+                    
 def shift_data_from_Playwright_Crawler_folder(src_dataset_folder_name, phishing_or_benign_tag):
     src_folder = os.path.join("..", "Playwright_Crawler", f"dataset_{src_dataset_folder_name}")
     dest_folder = os.path.join(phishing_or_benign_tag, "dataset")
