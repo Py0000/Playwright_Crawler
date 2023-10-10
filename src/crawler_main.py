74facded20d3b14eeb654f196e27f995f52f977f
Py0000 <pylim8266@gmail.com>
2023-10-11 01:19:20 +0800

    Add redirection count

--- src/crawler_main.py
+++ src/crawler_main.py
@@ -176,11 +176,11 @@ async def crawl(browser, url, dataset_folder_name, ref_flag):
         client.on("Network.requestWillBeSent", capture_request)
         timeout_task = asyncio.create_task(check_for_timeout())
 
-        page.on("response", on_response)
+        redirect_listener = page.add_listener("response", on_response)
         response = await page.goto(url, timeout=10000)
         await wait_for_page_to_load(page)
         visited_url = page.url # See if url changes after visiting the page.
-        page.off("response", on_response)
+        redirect_listener.remove_listener()
         if response:
             main_http_status = response.status
 
