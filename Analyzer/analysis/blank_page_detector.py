b31f8c586883f9fc5f50ec5b67a3032beeae6358
Py0000 <pylim8266@gmail.com>
2024-01-21 00:03:19 +0800

    Add potential script to detect inline css

--- Analyzer/analysis/blank_page_detector.py
+++ Analyzer/analysis/blank_page_detector.py
@@ -1,3 +1,4 @@
+import cssutils
 from bs4 import BeautifulSoup
 
 def read_html_script(html_file_path):
@@ -24,6 +25,14 @@ def detect_blank_page_html_script(html_file_path):
 
 # Possible to handle inline CSS Style + external CSS Style sheet? 
 # External CSS style sheet may need to make use of network resources downloaded when crawling and match those in the html script under <link> tags with rel="stylesheet"
-    
+
+def inline_css_hide_content(inline_css):
+    sheet = cssutils.parseString(inline_css)
+    for rule in sheet:
+        if rule.type == rule.STYLE_RULE:
+            for property in rule.style:
+                if property.name == 'display' and property.value == 'none':
+                    return True
+    return False
 
 # Possible to handle Javascript that causes a page to be blank?
