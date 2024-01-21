ea314929837b0ef5aa53ade68e18a6167682556e
Py0000 <pylim8266@gmail.com>
2024-01-21 17:25:23 +0800

    finalize css style blank detection

--- Analyzer/analysis/blank_page_detector.py
+++ Analyzer/analysis/blank_page_detector.py
@@ -5,8 +5,14 @@ import logging
 from bs4 import BeautifulSoup
 import os
 import zipfile
+import json
 
 cssutils.log.setLevel(logging.CRITICAL)
+
+def export_data_as_txt_file(filename, data):
+    with open(filename, 'w') as f:
+        for item in data:
+            f.write(str(item) + '\n')
 
 def read_html_script(html_file_path):
     with open(html_file_path, 'r', encoding='utf-8') as file:
@@ -33,9 +39,10 @@ def css_hide_content(css_content):
         sheet = cssutils.parseString(css_content)
         for rule in sheet:
             if rule.type == rule.STYLE_RULE:
-                for property in rule.style:
-                    if property.name == 'display' and property.value == 'none':
-                        return True
+                if 'body' in rule.selectorText or 'html' in rule.selectorText:
+                    for property in rule.style:
+                        if property.name == 'display' and property.value == 'none':
+                            return True
     return False
 
 # Recursively check external css scripts that renders the webpage blank
@@ -45,6 +52,7 @@ def external_css_hide_content(network_resp_path):
             with open(os.path.join(network_resp_path, filename), 'r', encoding='utf-8') as css_file:
                 css_content = css_file.read()
                 if css_hide_content(css_content):
+                    print("CSS File that has potential blank element: ", filename)
                     return True
     return False
 
@@ -58,15 +66,18 @@ def check_if_css_renders_blank(soup, network_resp_path):
     return is_blank_by_inline_css or is_blank_by_external_css
 
 
-# only checks html_script after js execution
+# Categorises html as blank if html_script_aft.html is blank
+# CSS and JS are being detected as backup measures (As the detection is not accurate)
 def check_dataset_for_blank(main_directory):
     consolidated_results = {}
-    blank_page_dataset = []
+    html_blank_page_dataset = []
+    css_blank_page_dataset = []
+
     extraction_path = main_directory.replace('.zip', '')
     date = extraction_path.split('_')[-1]
     
     with zipfile.ZipFile(main_directory, 'r') as zip_ref:
-        print("Extracting zip folder ...")
+        print("\nExtracting zip folder ...")
         zip_ref.extractall(extraction_path)
     
     parent_folder_path = os.path.join(extraction_path, f'dataset_{date}', f'dataset_{date}', 'complete_dataset')
@@ -80,28 +91,50 @@ def check_dataset_for_blank(main_directory):
             zip_ref.extractall(zip_extraction_path)
             current_dataset_dir = os.path.join(zip_extraction_path, current_dataset)
 
+        dataset_status = {}
         for sub_dir in os.listdir(current_dataset_dir):
             current_dataset_ref_dir = os.path.join(current_dataset_dir, sub_dir)
-            current_dataset_html_file = os.path.join(current_dataset_ref_dir, 'html_script_aft.html')
+            current_dataset_html_file_aft = os.path.join(current_dataset_ref_dir, 'html_script_aft.html')
+            current_dataset_html_file_bef = os.path.join(current_dataset_ref_dir, 'html_script_aft.html')
             current_dataset_nw_resp_dir = os.path.join(current_dataset_dir, sub_dir, 'network_response_files')
-            
-            html_content = read_html_script(current_dataset_html_file)
-            soup = BeautifulSoup(html_content, 'html.parser')
-            is_blank_by_html = detect_blank_page_html_script(soup)
 
-            if is_blank_by_html:
-                consolidated_results[f"{current_dataset} {sub_dir}"] = "Html script (after) renders blank"
-                blank_page_dataset.append(current_dataset)
+            html_content_bef = read_html_script(current_dataset_html_file_bef)
+            soup_bef = BeautifulSoup(html_content_bef, 'html.parser')
+            is_blank_by_html_bef = detect_blank_page_html_script(soup_bef)
             
-            is_blank_by_css = check_if_css_renders_blank(soup , current_dataset_nw_resp_dir)
+            html_content_aft = read_html_script(current_dataset_html_file_aft)
+            soup_aft = BeautifulSoup(html_content_aft, 'html.parser')
+            is_blank_by_html_aft = detect_blank_page_html_script(soup_aft)
+            is_blank_by_css = check_if_css_renders_blank(soup_aft , current_dataset_nw_resp_dir)
+
+            if is_blank_by_html_aft: 
+                html_blank_page_dataset.append(current_dataset)
             if is_blank_by_css:
-                consolidated_results[f"{current_dataset} {sub_dir}"] = "Css scripts renders blank"
-                blank_page_dataset.append(current_dataset)
+                css_blank_page_dataset.append(current_dataset)
 
-            consolidated_results[f"{current_dataset} {sub_dir}"] = "Not blank"
-            print(consolidated_results) 
+            status = {
+                "Html Script (Before)": "Blank" if is_blank_by_html_bef else "Not Blank",
+                "Html Script (After)": "Blank" if is_blank_by_html_aft else "Not Blank",
+                "CSS Style/Sheet": "Blank" if is_blank_by_css else "Not Blank",
+            }
+            dataset_status[sub_dir] = status
+        
+        consolidated_results[current_dataset] = dataset_status
+        print(consolidated_results) 
+        break
+    
 
-            break
+    html_blank_output = f"blank_page_logs/{date}_html_blank.txt"
+    css_blank_output = f"blank_page_logs/{date}_css_blank.txt"
+    consolidated_output = f"blank_page_logs/{date}_blank_consolidation.json"
+
+    with open(consolidated_output, 'w', encoding='utf-8') as f:
+        json.dump(consolidated_results, f, ensure_ascii=False, indent=4)
+    
+    export_data_as_txt_file(html_blank_output, html_blank_page_dataset)
+    export_data_as_txt_file(css_blank_output, css_blank_page_dataset)
+
+
 
 
 
