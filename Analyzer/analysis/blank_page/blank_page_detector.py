6f2909931c35f7d596ac0dbc351a8afc7b082648
Py0000 <pylim8266@gmail.com>
2024-01-26 00:07:24 +0800

    remove unnecessary code

--- Analyzer/analysis/blank_page/blank_page_detector.py
+++ Analyzer/analysis/blank_page/blank_page_detector.py
@@ -96,7 +96,7 @@ def check_dataset_for_blank(main_directory):
         
         consolidated_results[current_dataset] = dataset_status
         ss_stats[current_dataset] = ss_sub_stats   
-        break
+        
     
     base_output_dir = f"blank_page/primary_logs/{date}"
     if not os.path.exists(base_output_dir):
