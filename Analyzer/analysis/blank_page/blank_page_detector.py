d5df1d00ff9b6f692bfd4c26a38b44978e62911c
Py0000 <pylim8266@gmail.com>
2024-01-27 13:21:17 +0800

    improve script

--- Analyzer/analysis/blank_page/blank_page_detector.py
+++ Analyzer/analysis/blank_page/blank_page_detector.py
@@ -9,11 +9,11 @@ import blank_page_util
 from blank_page_secondary_detector import css_hide_content
 from image_analysis import is_screenshot_blank
 
-
-def read_html_script(html_file_path):
-    with open(html_file_path, 'r', encoding='utf-8') as file:
-        html_content = file.read()
-        return html_content
+def read_html_from_zip(zip_path, html_file_path):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        with zip_ref.open(html_file_path) as file:
+            html_content = file.read()
+            return html_content
 
 
 def detect_blank_page_html_script(soup):
@@ -25,78 +25,73 @@ def detect_blank_page_html_script(soup):
 
     # Check if content of <body> is empty
     # And check if there are any child (direct) tags inside <body>
-    if not body_content and len(soup.body.find_all(recursive=False)) == 0 and not is_inline_html_css_blank:
+    if not body_content and len(soup.body.find_all(recursive=False)) == 0 and is_inline_html_css_blank:
         return True
     else:
         return False
 
-# Categorises html as blank if html_script_aft.html is blank
-# CSS and JS are being detected as backup measures (As the detection is not accurate)
+
+def process_individual_dataset(current_dataset_dir, dir_without_zip_extension):
+    dataset_status = {}
+    ss_sub_stats = {}
+
+    for sub_dir in ["self_ref", "no_ref"]:
+        current_dataset_ref_dir = os.path.join(dir_without_zip_extension, sub_dir)
+    
+        current_dataset_html_file_aft = os.path.join(current_dataset_ref_dir, 'html_script_aft.html')
+        current_dataset_html_file_bef = os.path.join(current_dataset_ref_dir, 'html_script_bef.html')
+        current_dataset_ss_aft = os.path.join(current_dataset_ref_dir, 'screenshot_aft.png')
+        current_dataset_ss_bef = os.path.join(current_dataset_ref_dir, 'screenshot_bef.png')
+
+        html_content_bef = read_html_from_zip(current_dataset_dir, current_dataset_html_file_bef)
+        soup_bef = BeautifulSoup(html_content_bef, 'html.parser')
+        is_blank_by_html_bef = detect_blank_page_html_script(soup_bef)
+
+        html_content_aft = read_html_from_zip(current_dataset_dir, current_dataset_html_file_aft)
+        soup_aft = BeautifulSoup(html_content_aft, 'html.parser')
+        is_blank_by_html_aft = detect_blank_page_html_script(soup_aft)
+
+        is_ss_aft_blank, ss_aft_stats = is_screenshot_blank(current_dataset_dir, current_dataset_ss_aft)
+        is_ss_bef_blank, ss_bef_stats = is_screenshot_blank(current_dataset_dir, current_dataset_ss_bef)
+
+        status = {
+            "Html Script (Before)": "Blank" if is_blank_by_html_bef else "Not Blank",
+            "Html Script (After)": "Blank" if is_blank_by_html_aft else "Not Blank",
+            "Screenshot (After) result":  "Blank" if is_ss_aft_blank else "Not Blank",
+            "Screenshot (Before) result":  "Blank" if is_ss_bef_blank else "Not Blank",
+        }
+        dataset_status[sub_dir] = status
+
+        screenshot_stats = {
+            "After": ss_aft_stats,
+            "Before": ss_bef_stats
+        }
+        ss_sub_stats[sub_dir] = screenshot_stats
+    
+    return dataset_status, ss_sub_stats
+
+
+
 def check_dataset_for_blank(main_directory):
     consolidated_results = {}
     ss_stats = {}
 
     extraction_path = main_directory.replace('.zip', '')
     date = extraction_path.split('_')[-1]
-    
+
     with zipfile.ZipFile(main_directory, 'r') as zip_ref:
         print("\nExtracting zip folder ...")
         zip_ref.extractall(extraction_path)
     
     parent_folder_path = os.path.join(extraction_path, f'dataset_{date}', f'dataset_{date}', 'complete_dataset')
     for dir in os.listdir(parent_folder_path):
-        current_dataset = dir.replace('.zip', '')
+        print(f"Processing {dir}...")
+        dir_without_zip_extension = dir.replace('.zip', '')
         current_dataset_dir = os.path.join(parent_folder_path, dir)
-        
-        with zipfile.ZipFile(current_dataset_dir, 'r') as zip_ref:
-            zip_extraction_path = current_dataset_dir.replace('.zip', '')
-            print(f"Extracting inner zip folder {dir}...")
-            zip_ref.extractall(zip_extraction_path)
-            current_dataset_dir = os.path.join(zip_extraction_path, current_dataset)
+        dataset_stats, ss_sub_stats = process_individual_dataset(current_dataset_dir, dir_without_zip_extension)
+        consolidated_results[dir_without_zip_extension] = dataset_stats
+        ss_stats[dir_without_zip_extension] = ss_sub_stats
 
-        dataset_status = {}
-        ss_sub_stats = {}
-        for sub_dir in os.listdir(current_dataset_dir):
-            try:
-                current_dataset_ref_dir = os.path.join(current_dataset_dir, sub_dir)
-                current_dataset_html_file_aft = os.path.join(current_dataset_ref_dir, 'html_script_aft.html')
-                current_dataset_html_file_bef = os.path.join(current_dataset_ref_dir, 'html_script_bef.html')
-                
-                current_dataset_ss_aft = os.path.join(current_dataset_ref_dir, 'screenshot_aft.png')
-                current_dataset_ss_bef = os.path.join(current_dataset_ref_dir, 'screenshot_bef.png')
-
-                html_content_bef = read_html_script(current_dataset_html_file_bef)
-                soup_bef = BeautifulSoup(html_content_bef, 'html.parser')
-                is_blank_by_html_bef = detect_blank_page_html_script(soup_bef)
-                
-                html_content_aft = read_html_script(current_dataset_html_file_aft)
-                soup_aft = BeautifulSoup(html_content_aft, 'html.parser')
-                is_blank_by_html_aft = detect_blank_page_html_script(soup_aft)
-                
-                is_ss_aft_blank, ss_aft_stats = is_screenshot_blank(current_dataset_ss_aft)
-                is_ss_bef_blank, ss_bef_stats = is_screenshot_blank(current_dataset_ss_bef)
-
-                status = {
-                    "Html Script (Before)": "Blank" if is_blank_by_html_bef else "Not Blank",
-                    "Html Script (After)": "Blank" if is_blank_by_html_aft else "Not Blank",
-                    "Screenshot (After) result":  "Blank" if is_ss_aft_blank else "Not Blank",
-                    "Screenshot (Before) result":  "Blank" if is_ss_bef_blank else "Not Blank",
-                }
-                dataset_status[sub_dir] = status
-
-                screenshot_stats = {
-                    "After": ss_aft_stats,
-                    "Before": ss_bef_stats
-                }
-                ss_sub_stats[sub_dir] = screenshot_stats
-            except:
-                dataset_status[sub_dir] = "Error encountered while processing dataset folder"
-                ss_sub_stats[sub_dir] = "Error encountered while processing dataset folder"
-        
-        consolidated_results[current_dataset] = dataset_status
-        ss_stats[current_dataset] = ss_sub_stats   
-
-    
     base_output_dir = f"primary_logs/{date}"
     if not os.path.exists(base_output_dir):
         os.makedirs(base_output_dir)
@@ -112,7 +107,6 @@ def check_dataset_for_blank(main_directory):
     
     shutil.rmtree(extraction_path)
     return consolidated_output
-
 
 
 def get_error_logs(consolidated_log_file_path, date, base_output_dir):
@@ -143,6 +137,3 @@ if __name__ == '__main__':
     base_output_dir = os.path.join("primary_logs", date)
     blank_page_util.split_log_files(consolidated_output, date, ["html", "ss_aft", "ss_bef"], base_output_dir)
     get_error_logs(consolidated_output, date, base_output_dir)
-
-
-
--- Analyzer/analysis/blank_page/image_analysis.py
+++ Analyzer/analysis/blank_page/image_analysis.py
@@ -1,9 +1,45 @@
 from PIL import Image
 import numpy as np
+from io import BytesIO
 import cv2
 import pytesseract
+import zipfile
 
-def is_screenshot_blank(image_path, std_dev_threshold=6, edge_threshold=2000, text_threshold=10):
+def is_screenshot_blank(zip_path, image_path, std_dev_threshold=6, edge_threshold=2000, text_threshold=10):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        with zip_ref.open(image_path) as image_file:
+            image = Image.open(BytesIO(image_file.read()))
+
+             # Convert image to grey-scale
+            gray_image = np.array(image.convert('L'))
+            
+            # Calculate the standard deviation of the grayscale image
+            std_dev = np.std(gray_image)
+
+            # Use Canny edge detection to find edges in the image
+            # First threshold: 100 (Represents the lower boundary for pixel intensity gradient. If a pixel gradient is below this threshold, it is considered not to be an edge)
+            # Second threshold: 200 (Represents the upper boundary for pixel intensity gradient. If a pixel gradient is above this threshold, it is considered to be a strong edge.)
+            edges = cv2.Canny(gray_image, 100, 200)
+            edge_count = np.sum(edges > 0)
+
+            # Use OCR to detect text in the image
+            text = pytesseract.image_to_string(image)
+            text_count = len(text.strip())
+
+            stats = {
+                "std_dev": std_dev,
+                "edge_count": int(edge_count),
+                "text_count": int(text_count)
+            }
+
+            # Check against thresholds
+            if std_dev < std_dev_threshold and edge_count < edge_threshold and text_count < text_threshold:
+                return True, stats
+            
+            return False, stats
+
+
+def experiment_is_screenshot_blank(image_path, std_dev_threshold=6, edge_threshold=2000, text_threshold=10):
     image = Image.open(image_path)
 
     # Convert image to grey-scale
@@ -38,31 +74,31 @@ def is_screenshot_blank(image_path, std_dev_threshold=6, edge_threshold=2000, te
 # Only to be used to determine threshold values
 def experimentation_for_blank_page_threshold_values():
     print("\nNon-blank")
-    is_screenshot_blank("test_image_analysis/screenshot_aft.png")
+    experiment_is_screenshot_blank("test_image_analysis/screenshot_aft.png")
 
     print("\nGoogle")
-    is_screenshot_blank("test_image_analysis/Google.JPG")
+    experiment_is_screenshot_blank("test_image_analysis/Google.JPG")
 
     print("\nFacebook")
-    is_screenshot_blank("test_image_analysis/screenshot_fb.png")
+    experiment_is_screenshot_blank("test_image_analysis/screenshot_fb.png")
 
     print("\nBlank (dataset)")
-    is_screenshot_blank("test_image_analysis/screenshot_blank.png")
+    experiment_is_screenshot_blank("test_image_analysis/screenshot_blank.png")
 
     print("\nBlank with few words")
-    is_screenshot_blank("test_image_analysis/blank_with_few_words.JPG")
+    experiment_is_screenshot_blank("test_image_analysis/blank_with_few_words.JPG")
 
     print("\nBlank with simple words")
-    is_screenshot_blank("test_image_analysis/blank_with_simple_words.JPG")
+    experiment_is_screenshot_blank("test_image_analysis/blank_with_simple_words.JPG")
 
     print("\nBlank with grey line")
-    is_screenshot_blank("test_image_analysis/blank_with_grey_line.JPG")
+    experiment_is_screenshot_blank("test_image_analysis/blank_with_grey_line.JPG")
 
     print("\nBlank")
-    is_screenshot_blank("test_image_analysis/complete_blank.JPG")
+    experiment_is_screenshot_blank("test_image_analysis/complete_blank.JPG")
 
     print("\nBlank (Black)")
-    is_screenshot_blank("test_image_analysis/complete_blank_black.JPG")
+    experiment_is_screenshot_blank("test_image_analysis/complete_blank_black.JPG")
 
     print("\nBlank (White)")
-    is_screenshot_blank("test_image_analysis/complete_blank_white.JPG")
+    experiment_is_screenshot_blank("test_image_analysis/complete_blank_white.JPG")
