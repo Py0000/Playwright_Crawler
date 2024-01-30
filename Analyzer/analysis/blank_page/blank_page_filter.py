5f238c7b5535f424154953f731857583f0af08cc
Py0000 <pylim8266@gmail.com>
2024-01-30 23:36:51 +0800

    Refactor code

--- Analyzer/analysis/blank_page/blank_page_filter.py
+++ Analyzer/analysis/blank_page/blank_page_filter.py
@@ -1,136 +1,98 @@
-import argparse
 import os
 
 from Analyzer.utils import file_utils
 
-def read_blank_files_as_list(txt_file):
-    with open(txt_file, "r") as f: 
-        blank_folder_names = f.readlines()
-    
-    # Remove newline characters and any whitespace
-    blank_folder_list = [folder_name.strip() for folder_name in blank_folder_names]
+class BlankPageFilter:
+    def __init__(self, main_folder_path, date):
+        self.main_folder_path = main_folder_path
+        self.date = date
 
-    return blank_folder_list
-
-
-def filter_out_blank_page_by_html(date, dataset_path, blank_page_list, new_dir):
-    status = []
-
-    if "zip" in dataset_path:
-        dataset_path = file_utils.extract_zipfile(dataset_path)
-
-    parent_folder_path = os.path.join(dataset_path, f'dataset_{date}', f'dataset_{date}', 'complete_dataset')
-    
-    # Create the new directory to hold the dataset that contains the blank webpages
-    blank_page_dir = os.path.join(parent_folder_path, 'blank_pages', new_dir)
-    file_utils.check_and_generate_new_dir(blank_page_dir)
-
-
-    for folder in blank_page_list:
-        zip_dataset_path = os.path.join(parent_folder_path, f"{folder}.zip")
-        if (os.path.exists(zip_dataset_path)):
-            print(folder, os.path.exists(zip_dataset_path), "moved")
-            file_utils.shift_file_objects(zip_dataset_path, blank_page_dir)
-        else:
-            print(folder, os.path.exists(zip_dataset_path), "failed")
-            status.append(folder) 
-    
-    log_dir = f"cat_logs/{date}"
-    file_utils.check_and_generate_new_dir(log_dir)
-
-    output_path = os.path.join(log_dir, f"{date}_{new_dir}_categorization_failed.txt")
-    file_utils.export_output_as_txt_file(output_path, status)
-    return output_path
-
-
-def is_also_potentially_blank_by_other_files(log_dir_path, filtered_out_dataset, date, type):
-    print("Cross checking that filtered files have blank screenshots...")
-    status = {}
-
-    # Cross check against logs for screenshot
-    ss_aft_blank_list = read_blank_files_as_list(os.path.join(log_dir_path, f"{date}_ss_aft_blank_{type}.txt"))
-
-    # Return a dict {dataset_name: {css: true/false, js: true/false, ss_aft: true/false}}
-    for filtered in filtered_out_dataset:
-        is_ss_aft_blank = False
-
-        if filtered in ss_aft_blank_list:
-            is_ss_aft_blank = True
+    def read_blank_files_as_list(self, txt_file):
+        with open(txt_file, "r") as f: 
+            blank_folder_names = f.readlines()
         
-        data = {
-            "Is blank by ss (after)": is_ss_aft_blank
-        }
+        # Remove newline characters and any whitespace
+        blank_folder_list = [folder_name.strip() for folder_name in blank_folder_names]
 
-        status[filtered] = data
-    
-    log_dir = f"cat_logs/{date}"
-    output_path = os.path.join(log_dir, f"{date}_cat_cross_check_ss_{type}.json")
-    file_utils.export_output_as_json_file(output_path, status)
+        return blank_folder_list
 
 
-def potentially_blank_not_filtered_yet(log_dir_path, filtered_dataset, date, type):
-    print("Checking if filtered files are also potentially blank by other file types...")
-    ss_aft_blank_list = read_blank_files_as_list(os.path.join(log_dir_path, f"{date}_ss_aft_blank_{type}.txt"))
+    def filter_out_blank_page_by_html(self, blank_page_list, ref_type):
+        status = []
 
-    # Remove from ss_aft blank list/log files those that are already filtered
-    for filtered in filtered_dataset:
-        if filtered in ss_aft_blank_list:
-            ss_aft_blank_list.remove(filtered)
+        date = self.date
+        dataset_path = self.main_folder_path
+        if "zip" in dataset_path:
+            dataset_path = file_utils.extract_zipfile(dataset_path)
 
-    # Return a new log with the remaining unfiltered ones
-    log_dir = f"cat_logs/{date}"
-    output_file = os.path.join(log_dir, f"{date}_unfiltered_extra_ss_blank_{type}.txt")
-    file_utils.export_output_as_txt_file(output_file, ss_aft_blank_list)
+        parent_folder_path = os.path.join(dataset_path, f'dataset_{date}', f'dataset_{date}', 'complete_dataset')
+        
+        # Create the new directory to hold the dataset that contains the blank webpages
+        blank_page_dir = os.path.join(parent_folder_path, 'blank_pages', ref_type)
+        file_utils.check_and_generate_new_dir(blank_page_dir)
 
 
-def shift_logs_files(date, src_dir):
-    print(f"Shifting {src_dir} log files...")
-    logs_dir = os.path.join(f"dataset_{date}", f"dataset_{date}", f"dataset_{date}", "complete_dataset", "blank_pages", "logs", src_dir)
-    file_utils.check_and_generate_new_dir(logs_dir)
-    
-    for file in os.listdir(os.path.join(src_dir, date)):
-        src_file_path = os.path.join(os.path.join(src_dir, date), file)
-        if os.path.exists(src_file_path):
-            dest_file_path = os.path.join(logs_dir, file)
-            file_utils.shift_file_objects(src_file_path, dest_file_path)
-        else:
-            print(f"File {file} does not exist!")
-    
+        for folder in blank_page_list:
+            zip_dataset_path = os.path.join(parent_folder_path, f"{folder}.zip")
+            if (os.path.exists(zip_dataset_path)):
+                print(folder, os.path.exists(zip_dataset_path), "moved")
+                file_utils.shift_file_objects(zip_dataset_path, blank_page_dir)
+            else:
+                print(folder, os.path.exists(zip_dataset_path), "failed")
+                status.append(folder) 
+        
+        log_dir = f"Analyzer/analysis/blank_page/cat_logs/{date}"
+        file_utils.check_and_generate_new_dir(log_dir)
 
-def clean_up_logs(date):
-    log_files_folder = [f'primary_logs', f'cat_logs']
-    for folder in log_files_folder:
-        shift_logs_files(date, folder)
+        output_path = os.path.join(log_dir, f"{date}_{ref_type}_categorization_failed.txt")
+        file_utils.export_output_as_txt_file(output_path, status)
+        return output_path
 
 
 
+class BlankPageFilterChecker:
+    def __init__(self, primary_logs_dir, date):
+        self.primary_logs_dir = primary_logs_dir
+        self.date = date
+        
+    def cross_check_that_ss_is_blank(self, filtered_out_dataset, type):
+        print("Cross checking that filtered files have blank screenshots...")
+        status = {}
+        date = self.date
+        # Cross check against logs for screenshot
+        ss_aft_blank_list = self.read_blank_files_as_list(os.path.join(self.primary_logs_dir, f"{date}_ss_aft_blank_{type}.txt"))
+
+        # Return a dict {dataset_name: {css: true/false, js: true/false, ss_aft: true/false}}
+        for filtered in filtered_out_dataset:
+            is_ss_aft_blank = False
+
+            if filtered in ss_aft_blank_list:
+                is_ss_aft_blank = True
+            
+            data = {
+                "Is blank by ss (after)": is_ss_aft_blank
+            }
+
+            status[filtered] = data
+        
+        log_dir = f"Analyzer/analysis/blank_page/cat_logs/{date}"
+        output_path = os.path.join(log_dir, f"{date}_cat_cross_check_ss_{type}.json")
+        file_utils.export_output_as_json_file(output_path, status)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Supply the folder names")
-    parser.add_argument("date", help="Date")
-    parser.add_argument("folder_path", help="Folder name")
-    parser.add_argument("blank_txt", help="Blank Page File")
-    #parser.add_argument("new_dir", help="Name of dir to keep blank dataset")
-    args = parser.parse_args()
+    def potentially_ss_blank_not_filtered_yet(self, filtered_dataset, type):
+        print("Checking if filtered files are also potentially blank by other file types...")
+        date = self.date
+        ss_aft_blank_list = self.read_blank_files_as_list(os.path.join(self.primary_logs_dir, f"{date}_ss_aft_blank_{type}.txt"))
 
-    # Single
-    types = ["both", "self_ref", "no_ref"]
-    for t in types:
-        log_dir_path = os.path.join("primary_logs", args.date)
-        blank_txt_path = os.path.join(log_dir_path, f"{args.date}_{args.blank_txt}_{t}.txt")
-        blank_page_list = read_blank_files_as_list(blank_txt_path)
+        # Remove from ss_aft blank list/log files those that are already filtered
+        for filtered in filtered_dataset:
+            if filtered in ss_aft_blank_list:
+                ss_aft_blank_list.remove(filtered)
 
-        if t != "both":
-            folder_path = args.folder_path.replace(".zip", "")
-        else:
-            folder_path = args.folder_path
-        unsuccessful_filtered_path = filter_out_blank_page_by_html(args.date, folder_path, blank_page_list, "both")
+        # Return a new log with the remaining unfiltered ones
+        log_dir = f"Analyzer/analysis/blank_page/cat_logs/{date}"
+        output_file = os.path.join(log_dir, f"{date}_unfiltered_extra_ss_blank_{type}.txt")
+        file_utils.export_output_as_txt_file(output_file, ss_aft_blank_list)
 
-        unsuccessful_filtered = read_blank_files_as_list(unsuccessful_filtered_path)
-        filtered = [item for item in blank_page_list if item not in unsuccessful_filtered]
-        is_also_potentially_blank_by_other_files(log_dir_path, filtered, args.date, "both")
-        potentially_blank_not_filtered_yet(log_dir_path, filtered, args.date, "both")
-    
-    clean_up_logs(args.date)
 
--- Analyzer/analysis/blocked_page_filter.py
+++ Analyzer/analysis/blocked_page_filter.py
@@ -3,69 +3,75 @@ import os
 
 from Analyzer.utils import file_utils
 
-def get_seperate_list_of_blocked_data(txt_file):
-    data = file_utils.read_data_from_json_file(txt_file)
-    
-    both = []
-    self_ref = []
-    no_ref = []
+class BlockedPageFilter:
+    def __init__(self, main_folder_path):
+        self.main_folder_path = main_folder_path
+        
 
-    for key, value in data.items():
-        self_ref_value = value.get('self_ref')
-        no_ref_value = value.get('no_ref')
+    def get_seperate_list_of_blocked_data(self, txt_file):
+        data = file_utils.read_data_from_json_file(txt_file)
+        
+        both = []
+        self_ref = []
+        no_ref = []
 
-        if self_ref_value != 200 and no_ref_value != 200:
-            both.append(key)
-        elif self_ref_value != 200:
-            self_ref.append(key)
-        elif no_ref_value != 200:
-            no_ref.append(key)
-    
-    blocked_lists_dict = {
-        "both": both,
-        "self_ref": self_ref, 
-        "no_ref": no_ref
-    }
+        for key, value in data.items():
+            self_ref_value = value.get('self_ref')
+            no_ref_value = value.get('no_ref')
 
-    return blocked_lists_dict
+            if self_ref_value != 200 and no_ref_value != 200:
+                both.append(key)
+            elif self_ref_value != 200:
+                self_ref.append(key)
+            elif no_ref_value != 200:
+                no_ref.append(key)
+        
+        blocked_lists_dict = {
+            "both": both,
+            "self_ref": self_ref, 
+            "no_ref": no_ref
+        }
 
-
-def filter_blocked_page_by_category(date, parent_path, blocked_dir, cat, blocked_list):
-    status = []
-
-    blocked_sub_dir = os.path.join(blocked_dir, cat)
-    file_utils.check_and_generate_new_dir(blocked_sub_dir)
-    
-    for folder in blocked_list:
-        zip_dataset_path = os.path.join(parent_path, folder)
-        if (os.path.exists(zip_dataset_path)):
-            file_utils.shift_file_objects(zip_dataset_path, blocked_sub_dir)
-        else:
-            status.append(folder)
-    
-    output_path = f"{date}_blocked_page_cat_failed.txt"
-    file_utils.export_output_as_txt_file(output_path, status)
+        return blocked_lists_dict
 
 
-def filter_blocked_page_main(main_directory):
-    if "zip" in main_directory:
-        main_directory = file_utils.extract_zipfile(main_directory)
+    def filter_blocked_page_by_category(self, date, parent_path, blocked_dir, cat, blocked_list):
+        status = []
 
-    date = file_utils.extract_date_from_extracted_zipfile_name(main_directory)
+        blocked_sub_dir = os.path.join(blocked_dir, cat)
+        file_utils.check_and_generate_new_dir(blocked_sub_dir)
+        
+        for folder in blocked_list:
+            zip_dataset_path = os.path.join(parent_path, folder)
+            if (os.path.exists(zip_dataset_path)):
+                file_utils.shift_file_objects(zip_dataset_path, blocked_sub_dir)
+            else:
+                status.append(folder)
+        
+        output_path = f"{date}_blocked_page_cat_failed.txt"
+        file_utils.export_output_as_txt_file(output_path, status)
 
-    txt_parent_folder_path = os.path.join(main_directory, f'dataset_{date}', 'filter_logs')
-    parent_path = os.path.join(main_directory, f'dataset_{date}', f'dataset_{date}', 'complete_dataset')
 
-    blocked_dataset_txt_file_path = os.path.join(txt_parent_folder_path, f"{date}_response_non_200_status.json")
-    blocked_lists_dict = get_seperate_list_of_blocked_data(blocked_dataset_txt_file_path)
-    
-    blocked_page_dir = os.path.join(parent_path, "blocked")
-    file_utils.check_and_generate_new_dir(blocked_page_dir)
-    
-    blocked_cats = ["both", "self_ref", "no_ref"]
-    for cat in blocked_cats:
-        print(f"Filtering based on {cat}...")
-        filter_blocked_page_by_category(date, parent_path, blocked_page_dir, cat, blocked_lists_dict.get(cat))
+    def filter_blocked_page_main(self):
+        main_directory = self.main_folder_path
+        if "zip" in main_directory:
+            main_directory = file_utils.extract_zipfile(main_directory)
+
+        date = file_utils.extract_date_from_extracted_zipfile_name(main_directory)
+
+        txt_parent_folder_path = os.path.join(main_directory, f'dataset_{date}', 'filter_logs')
+        parent_path = os.path.join(main_directory, f'dataset_{date}', f'dataset_{date}', 'complete_dataset')
+
+        blocked_dataset_txt_file_path = os.path.join(txt_parent_folder_path, f"{date}_response_non_200_status.json")
+        blocked_lists_dict = self.get_seperate_list_of_blocked_data(blocked_dataset_txt_file_path)
+        
+        blocked_page_dir = os.path.join(parent_path, "blocked")
+        file_utils.check_and_generate_new_dir(blocked_page_dir)
+        
+        blocked_cats = ["both", "self_ref", "no_ref"]
+        for cat in blocked_cats:
+            print(f"Filtering based on {cat}...")
+            self.filter_blocked_page_by_category(date, parent_path, blocked_page_dir, cat, blocked_lists_dict.get(cat))
     
 
 
@@ -74,4 +80,5 @@ if __name__ == '__main__':
     parser.add_argument("folder_path", help="Folder name")
     args = parser.parse_args()
 
-    filter_blocked_page_main(args.folder_path)
+    block_page_filterer = BlockedPageFilter(args.folder_path)
+    block_page_filterer.filter_blocked_page_main()
